c7c441ba867b54ce41ae0180ac3bb42f0d3044b5
Adenilson Santos <adenilson.santos@gocase.com.br>
2023-02-09 00:25:24 -0300

    fix

--- index.cs
+++ index.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Windows.Forms;
-using ACR122U_Helper;
+using ACR122U.NET;
 using System.Drawing;
 using System.Text;
 
@@ -8,92 +8,75 @@ namespace NFC_Software
 {
     public partial class Form1 : Form
     {
-        private ACR122U _reader;
-        private bool _deviceConnected;
+    private ACR122U _reader;
+    private bool _deviceConnected;
+    public Form1()
+    {
+        InitializeComponent();
+    }
 
-        public Form1()
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        _reader = new ACR122U();
+
+        // Verifica se o dispositivo NFC está conectado
+        if (_reader.IsConnected)
         {
-            InitializeComponent();
+            _deviceConnected = true;
+            labelDeviceStatus.Text = "Conectado";
+            labelDeviceStatus.ForeColor = Color.Green;
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        else
         {
-            _reader = new ACR122U();
-            _reader.CardInserted += reader_CardInserted;
-            _reader.CardRemoved += reader_CardRemoved;
-
-            // Verifica se o dispositivo NFC está conectado
-            if (_reader.IsConnected)
-            {
-                _deviceConnected = true;
-                labelDeviceStatus.Text = "Conectado";
-                labelDeviceStatus.ForeColor = Color.Green;
-            }
-            else
-            {
-                _deviceConnected = false;
-                labelDeviceStatus.Text = "Desconectado";
-                labelDeviceStatus.ForeColor = Color.Red;
-            }
-        }
-
-        private void reader_CardInserted(object sender, EventArgs e)
-        {
-            labelCardStatus.Text = "Cartão Conectado";
-            labelCardStatus.ForeColor = Color.Green;
-        }
-
-        private void reader_CardRemoved(object sender, EventArgs e)
-        {
-            labelCardStatus.Text = "Cartão Desconectado";
-            labelCardStatus.ForeColor = Color.Red;
-        }
-
-        private void buttonFormat_Click(object sender, EventArgs e)
-        {
-            if (_deviceConnected)
-            {
-                // Formata o cartão conectado
-                _reader.Format();
-                MessageBox.Show("Cartão formatado com sucesso");
-            }
-            else
-            {
-                MessageBox.Show("Dispositivo NFC não conectado");
-            }
-        }
-
-        private void buttonWrite_Click(object sender, EventArgs e)
-        {
-            if (_deviceConnected)
-            {
-                // Verifica se há um cartão conectado
-                if (_reader.IsCardPresent)
-                {
-                    // Lê o QR Code da câmera
-                    var qrCode = ReadQRCodeFromCamera();
-
-                    // Escreve a URL no cartão
-                    _reader.WriteUrl(qrCode);
-                    MessageBox.Show("URL escrita com sucesso no cartão");
-                }
-                else
-                {
-                    MessageBox.Show("Nenhum cartão conectado");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Dispositivo NFC não conectado");
-            }
-        }
-
-        private string ReadQRCodeFromCamera()
-        {
-            // Coloque aqui o código para ler o QR Code da câmera
-            // Exemplo:
-            string qrCode = "https://www.example.com";
-            return qrCode;
+            _deviceConnected = false;
+            labelDeviceStatus.Text = "Desconectado";
+            labelDeviceStatus.ForeColor = Color.Red;
         }
     }
+
+    private void buttonFormat_Click(object sender, EventArgs e)
+    {
+        if (_deviceConnected)
+        {
+            // Formata o cartão conectado
+            _reader.Format();
+            MessageBox.Show("Cartão formatado com sucesso");
+        }
+        else
+        {
+            MessageBox.Show("Dispositivo NFC não conectado");
+        }
+    }
+
+    private void buttonWrite_Click(object sender, EventArgs e)
+    {
+        if (_deviceConnected)
+        {
+            // Verifica se há um cartão conectado
+            if (_reader.IsCardPresent)
+            {
+                // Lê o QR Code da câmera
+                var qrCode = ReadQRCodeFromCamera();
+
+                // Escreve a URL no cartão
+                _reader.WriteUrl(qrCode);
+                MessageBox.Show("URL escrita com sucesso no cartão");
+            }
+            else
+            {
+                MessageBox.Show("Nenhum cartão conectado");
+            }
+        }
+        else
+        {
+            MessageBox.Show("Dispositivo NFC não conectado");
+        }
+    }
+
+    private string ReadQRCodeFromCamera()
+    {
+        string qrCode = "https://www.example.com";
+        return qrCode;
+    }
+}
 }
